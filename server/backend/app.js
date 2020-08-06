7c25072a32ae5c1a8b4d7f34ebd133c7d1a53b48
HardBird <67629949+HardBird@users.noreply.github.com>
2020-08-06 22:05:30 +0900

    NOT SOLVE CORS

--- server/backend/app.js
+++ server/backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors=require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/posts',postsRouter)
+app.use('/posts',postsRouter);
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,7 +36,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  res.set({'access-control-allow-origin': '*'});
   // render the error page
   res.status(err.status || 500);
   res.render('error');
